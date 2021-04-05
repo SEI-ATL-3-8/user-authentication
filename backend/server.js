2a1b73084a3470f69d6e3aad26e1c4e0c4803372
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-05 12:31:23 -0400

    no users#create

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,6 @@ app.use(require('cors')())
 
 const models = require('./models')
 
-app.post('/users', async (req, res) => {
-  try {
-    const user = await models.user.create(req.body)
-    res.json(user.id)
-  } catch (error) {
-    if (error.errors && error.errors[0].message === 'email must be unique') {
-      res.status(400).json({error: 'email is already taken'})
-    } else {
-      res.status(400).json({error})
-    }
-  }
-})
 
 
 
