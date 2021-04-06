91bfd94072fea901361c95ef70f6d7005720ffff
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-06 13:59:44 -0400

    userId in localStorage

--- frontend/main.js
+++ frontend/main.js
@@ -23,3 +23,42 @@ document.querySelector('#profile-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#profile-content').classList.remove('hidden')
 })
+
+// form submission
+document.querySelector('#signup-form').addEventListener('submit', async (event) => {
+  event.preventDefault()
+  
+  const email = document.querySelector('#signup-email').value
+  const password = document.querySelector('#signup-password').value
+
+  try {
+    const response = await axios.post('http://localhost:3001/users', {
+      email: email,
+      password: password
+    })
+
+    console.log(response)
+
+    const userId = response.data.user.id
+    localStorage.setItem('userId', userId)
+
+  } catch (error) {
+    console.log(error)
+    alert('email is already taken')
+  }
+
+  
+  // make a request
+  // POST http://localhost:3001/users
+  // send a body
+  // email & password
+  // get em from the inputs, use .value
+})
+
+
+
+
+
+
+
+
