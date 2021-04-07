538655ebb59a032b5134f2db07c69112b9e53f31
Natalie <theliteshado@icloud.com>
2021-04-07 09:51:20 -0400

    user auth

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,25 @@ app.use(require('cors')())
 
 const models = require('./models')
 
+const createUser = async (req,res) => {
+  try {
+      const user = await models.user.create({
+          email: req.body.email,
+          password: req.body.password,
+      })
+
+      res,json({ message: 'ok', user})
+  } catch (error) {
+      res.status(400)
+      res.json({ error: 'email already taken'})
+  }
+}
+
+app.post('./users', createUser)
+
+
+
 const PORT = process.env.port || 3001
 app.listen(PORT, () => {
   routesReport.print()
 })
-
--- frontend/main.js
+++ frontend/main.js
@@ -23,3 +23,29 @@ document.querySelector('#profile-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#profile-content').classList.remove('hidden')
 })
+
+
+document.querySelector('#signup-form').addEventListener('submit', async (event) => {
+  event.preventDefault()
+
+  const email = document.querySelector('#signup-email').value
+  const password = document.querySelector('#signup-email').value
+  console.log(email)
+  console.log(password)
+
+  try {
+
+  const response = await axios.post('http://localhost:3001/users', {
+    email: email,
+    password: password
+  })
+
+  console.log(response)
+
+  const userId = response.data.user.id
+  localStorage.setItem('userId', userId)
+} catch (error) {
+  console.log(error)
+  alert('email taken')
+}
+})
